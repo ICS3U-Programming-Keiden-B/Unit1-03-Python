d98427b7791a9c8ce0a4a21453f5f43ed83b8906
Ubuntu <ubuntu@ip-172-31-25-78.ec2.internal>
2021-11-25 18:01:49 +0000

    Day 3: Hello world with pystyle

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Keiden
+# Date: Nov. 25, 2021
+# This program displays Hello World! with proper style.
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
